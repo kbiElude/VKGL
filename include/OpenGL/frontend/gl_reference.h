9bf2885a6c6a65e74bc6b53984bdebb25117dc03
Dominik Witczak <kbi@theelude.eu>
2018-10-06 22:35:26 +0200

    (frontend) Move GL payload and reference declaration to frontend, leave the common part under OpenGL/

--- include/OpenGL/frontend/gl_reference.h
+++ include/OpenGL/frontend/gl_reference.h
@@ -6,10 +6,7 @@
 #define VKGL_GL_REFERENCE_H
 
 #include "GL/glcorearb.h"
-#include <chrono>
-#include <functional>
-#include <memory>
-
+#include "OpenGL/reference.h"
 
 namespace OpenGL
 {
@@ -19,70 +16,6 @@ namespace OpenGL
     typedef std::chrono::high_resolution_clock::time_point TimeMarker;
 
     extern const OpenGL::TimeMarker LATEST_SNAPSHOT_AVAILABLE;
-
-    template<typename Payload>
-    class ReferenceBase
-    {
-    public:
-        /* Public functions */
-
-        typedef std::unique_ptr<ReferenceBase<Payload>, std::function<void(ReferenceBase<Payload>*)> > ReferenceUniquePtr;
-
-        ReferenceBase(const Payload&                                                in_payload,
-                      std::function<void(ReferenceBase<Payload>*) >                 in_on_reference_created_func,
-                      std::function<void(ReferenceBase<Payload>*) >                 in_on_reference_destroyed_func,
-                      std::function<ReferenceUniquePtr(GLuint, OpenGL::TimeMarker)> in_acquire_reference_func)
-            :m_acquire_reference_func     (in_acquire_reference_func),
-             m_on_reference_created_func  (in_on_reference_created_func),
-             m_on_reference_destroyed_func(in_on_reference_destroyed_func),
-             m_payload                    (in_payload)
-        {
-            m_on_reference_created_func(this);
-        }
-
-        ~ReferenceBase()
-        {
-            if (m_on_reference_destroyed_func != nullptr)
-            {
-                m_on_reference_destroyed_func(this);
-            }
-        }
-
-        ReferenceUniquePtr clone() const
-        {
-            /* When cloning, make sure to create a reference that points to exactly the same snapshot
-             * of the same object this reference is pointing at!
-             */
-            return m_acquire_reference_func(m_payload.id,
-                                            m_payload.time_marker);
-        }
-
-        const Payload& get_payload() const
-        {
-            return m_payload;
-        }
-
-        bool operator==(const ReferenceBase<Payload>& in_ref) const
-        {
-            return (m_payload == in_ref.m_payload);
-        }
-
-        bool operator!=(const ReferenceBase<Payload>& in_ref) const
-        {
-            return (m_payload != in_ref.m_payload);
-        }
-    private:
-
-        ReferenceBase<Payload>           (const ReferenceBase<Payload>&);
-        ReferenceBase<Payload>& operator=(const ReferenceBase<Payload>&);
-
-        /* Private variables */
-        std::function<ReferenceUniquePtr(GLuint, OpenGL::TimeMarker)> m_acquire_reference_func;
-        std::function<void(ReferenceBase<Payload>*) >                 m_on_reference_created_func;
-        std::function<void(ReferenceBase<Payload>*) >                 m_on_reference_destroyed_func;
-
-        const Payload m_payload;
-    };
 
     typedef struct GLPayload
     {
