7700643710a0a3e933353cf313e9ad09dbfe1af5
Dominik Witczak <kbi@theelude.eu>
2018-10-07 23:19:23 +0200

    (backend) Introduce pieces related to fundamental frame graph design

--- include/OpenGL/backend/vk_frame_graph.h
+++ include/OpenGL/backend/vk_frame_graph.h
@@ -7,95 +7,12 @@
 
 #include "Anvil/include/misc/types.h"
 #include "OpenGL/backend/vk_commands.h"
+#include "OpenGL/backend/vk_frame_graph_node.h"
 #include "OpenGL/types.h"
 #include <map>
 
 namespace OpenGL
 {
-    enum class NodeIOType
-    {
-        Buffer,
-        Image
-    };
-
-    typedef struct NodeIO
-    {
-        VKBufferReferenceUniquePtr buffer_reference_ptr;
-        // VKImageReferenceUniquePtr image_reference_ptr; - todo
-        const NodeIOType           type;
-
-        union
-        {
-            struct BufferProps
-            {
-                VkDeviceSize size;
-                VkDeviceSize start_offset;
-
-                BufferProps()
-                {
-                    /* Stub */
-                }
-
-                BufferProps(const VkDeviceSize& in_start_offset,
-                            const VkDeviceSize& in_size)
-                    :size        (in_size),
-                     start_offset(in_start_offset)
-                {
-                    /* Stub */
-                }
-            } buffer_props;
-
-            struct ImageProps
-            {
-                Anvil::ImageSubresourceRange subresource_range;
-
-                ImageProps()
-                {
-                    /* Stub */
-                }
-                ImageProps(const Anvil::ImageSubresourceRange& in_subresource_range)
-                    :subresource_range(subresource_range)
-                {
-                    /* Stub */
-                }
-            } image_props;
-        };
-
-        explicit NodeIO(VKBufferReferenceUniquePtr in_vk_buffer_reference_ptr,
-                        const VkDeviceSize&        in_start_offset,
-                        const VkDeviceSize&        in_size)
-            :buffer_props        (in_start_offset,
-                                  in_size),
-             buffer_reference_ptr(std::move(in_vk_buffer_reference_ptr) ),
-             type                (NodeIOType::Buffer)
-        {
-            /* Stub */
-        }
-
-#if 0
-        TODO
-
-        explicit NodeIO(VKImageReferenceUniquePtr           in_vk_image_reference_ptr,
-                        const Anvil::ImageSubresourceRange& in_subresource_range)
-            :image_props        (in_subresource_range),
-             image_reference_ptr(std::move(in_vk_image_reference_ptr) ),
-             type               (NodeIOType::Image)
-        {
-            /* Stub */
-        }
-#endif
-
-    } NodeIO;
-
-    struct VKFrameGraphNode
-    {
-        CommandBaseUniquePtr command_ptr;
-        std::vector<NodeIO>  inputs;
-        std::vector<NodeIO>  outputs;
-    };
-
-    typedef std::unique_ptr<VKFrameGraphNode, std::function<void(VKFrameGraphNode*)> > VKFrameGraphNodeUniquePtr;
-
     class VKFrameGraph
     {
     public:
